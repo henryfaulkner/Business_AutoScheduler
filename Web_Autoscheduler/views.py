2ff883656c89968cf8c05d09b6ad25f9e90b2a5f
Charlie <charles.w.marlow@gmail.com>
2019-12-17 13:18:52 -0500

    added validation

--- Web_Autoscheduler/views.py
+++ Web_Autoscheduler/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+def validate_email(email: str) -> bool:
+    return re.match(
+        '^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*'
+        '@'
+        '(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$',
+        email) is not None
+
 # Create your views here.
 def index(request):
     return HttpResponse("Hello, world. You're at the AutoScheduler index.")
